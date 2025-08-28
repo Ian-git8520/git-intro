253c83f9f3eb213087eb8ff121b4244f2c92f73d
ian_git8520 <iandavidnjenga@gmail.com>
2025-08-28 14:36:42 +0300

    updated

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const students = [
+    {
+        name: "Moringa",
+        age: 45,
+        gender: "F"
+    },
+    {
+        name: "Tomashi",
+        age: 50,
+        gender: "M"
+    },
+    {
+        name: "Molly",
+        age: 35,
+        gender: "F"
+    }
+]
+
+
+
+// Our business logic starts here
+function calculateYOB(currentYear, currentAge){
+    const YOB= 2025 - ["age"];
+    return YOB
+}
+
+//our UI logic starts here
+function displaycalculateYOB(){
+    console.log(calculateYOB(2025 - ["age"]))}
+    displaycalculateYOB()
